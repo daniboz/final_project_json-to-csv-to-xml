0162f13384763c5bc554e01f67b13a37f36af09c
danielbozesan <103513227+danielbozesan@users.noreply.github.com>
2022-09-12 12:28:56 +0300

    Initial commit- json-to-csv

---
+++ src/main/java/Main.java
@@ -0,0 +1,26 @@
+
+
+import org.apache.log4j.BasicConfigurator;
+import test.CSVWriter;
+import test.JSONFlattener;
+
+import java.io.File;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        List<Map<String, String>> flatJson;
+
+        flatJson = JSONFlattener.parseJson(new File("test.json"), "UTF-8");
+
+        Set<String> header = CSVWriter.collectOrderedHeaders(flatJson);
+
+        CSVWriter.writeLargeFile(flatJson, ";", "test.csv", header);
+        BasicConfigurator.configure();
+    }
+    
+}
