f8c1d1e5dbec73f3821e79f41de8065f4317e143
danielbozesan <103513227+danielbozesan@users.noreply.github.com>
2022-09-13 18:22:36 +0300

    json to csv or json to xml or xml to json

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,78 +1,26 @@
 
+import java.util.Scanner;
 
-
-import test.CSVWriter;
-import test.JSONFlattener;
-
-import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.json.XML;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
 
-        List<Map<String, String>> flatJson;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the path of the file you want to convert: ");
+        String pathConverting = scanner.nextLine();
+        System.out.println("Enter the path of the file where you want to get the converted outcome");
+        String pathConverted = scanner.nextLine();
 
-        flatJson = JSONFlattener.parseJson(new File("test.json"), "UTF-8");
+        TypeOfFile converter = new TypeOfFile(pathConverting, pathConverted);
+        converter.cases();
 
-        Set<String> header = CSVWriter.collectOrderedHeaders(flatJson);
 
-        CSVWriter.writeLargeFile(flatJson, ";", "test.csv", header);
-
-        // create variable loc that store location of the Sample.json file
-        String loc = "C:\\Users\\Dani\\Desktop\\New Folder\\test.json";
-
-        String result;
-        try {
-
-            // read byte data from the Sample.json file and convert it into String
-            result = new String(Files.readAllBytes(Paths.get(loc)));
-            //Convert JSON to XML
-            String xml = convertToXML(result, "root"); // This method converts json object to xml string
-
-            // use try-catch to store XML data into file.
-            FileWriter file = new FileWriter("C:\\Users\\Dani\\Desktop\\XMLData.txt");
-
-            // use write() method of File to write XML data into XMLData.txt
-            file.write(xml);
-            file.flush();
-            System.out.println("Your XML data is successfully written into XMLData.txt");
-
-            // close FileWriter
-            file.close();
-
-        } catch (IOException e1) {
-            // TODO Auto-generated catch block
-            e1.printStackTrace();
-        }
+//        C:\\Users\\Dani\\Desktop\\tasks_hibyte\\json-to-csv-to-xml\\TASK\\test.json
+//        C:\\Users\\Dani\\Desktop\\test1.json
+//        C:\\Users\\Dani\\Desktop\\testxml.txt
+//        C:\\Users\\Dani\\Desktop\\tasks_hibyte\\json-to-csv-to-xml\\final_project_json-to-csv-to-xml\\XMLData.txt
 
     }
-    public static String convertToXML(String jsonString, String root) throws JSONException {    // handle JSONException
-
-        try {
-            JSONObject jsonObject = new JSONObject(jsonString);
-
-            // use XML.toString() method to convert JSON into XML and store the result into a string
-            String xml = root+">" + XML.toString(jsonObject) + "</"+root+">";
-            return xml;
-
-        } catch (JSONException e) {
-            JSONArray jsonArray = new JSONArray(jsonString);
-
-            // use XML.toString() method to convert JSON into XML and store the result into a string
-            String xml = root + ">" + XML.toString(jsonArray) + "</" + root + ">";
-            return xml;
-        }
-    }
-
 
 }
